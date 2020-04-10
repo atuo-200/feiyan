d565526a83e2e54739e0393b9a7a3f852d74c36c
atuo-200 <59106812+atuo-200@users.noreply.github.com>
2020-04-10 17:18:23 +0800

    '第三次提交'

--- task1/task1.py
+++ task1/task1.py
@@ -1,9 +1,9 @@
 """
-本模块为爬虫模块，使用动态爬虫中的逆向分析法爬取疫情相关的数据，并保存成json文件
+本模块为爬虫模块，使用动态爬虫中的逆向分析法爬取疫情相关的数据，并保存成csv文件
 """
 import json
 import requests
-import pandas
+import pandas as pd
 #爬取数据
 def spider_data(url):
 	headers = {
@@ -16,7 +16,7 @@ def spider_data(url):
 #爬取海外疫情数据
 overseas_data = spider_data("https://api.inews.qq.com/newsqa/v1/automation/foreign/country/ranklist")
 overseas_data= [(i["name"],i["confirmAdd"],i["confirm"],i["dead"],i["heal"]) for i in overseas_data]
-overseas_data = pandas.DataFrame(overseas_data)
+overseas_data = pd.DataFrame(overseas_data)
 new_cols1 = ['国家', '新增确诊','累计确诊','治愈', '死亡']
 overseas_data.columns = new_cols1
 overseas_data.to_csv('overseas_data.csv', encoding='utf-8')
@@ -31,21 +31,28 @@ city_total_list = []
 for i in range(len(china_city_data)):
 	province = china_city_data[i]['name']
 	province_list = china_city_data[i]['children']
-	city_list = [(a["name"],province,a["total"],a["today"])for a in province_list]
+	city_list = [(province,a["name"],a["total"])for a in province_list]
 	city_total_list.extend(city_list)
-city_total_list = pandas.DataFrame(city_total_list)
-new_cols2 = ['城市', '省份','共计','新增']
+city_total_list = pd.DataFrame(city_total_list)
+new_cols2 = ['省份', '城市','共计']
 city_total_list.columns = new_cols2
-city_total_list.to_csv('city_total_list.csv', encoding='utf-8')
 
+#国内疫情数据二次预处理
+total_data = pd.DataFrame(city_total_list["共计"].values.tolist())[["nowConfirm","confirm","dead","heal"]]
+city_total_list = pd.concat([city_total_list,total_data],axis = 1)
+city_total_list.drop('共计',axis=1, inplace=True)
+
+#保存数据
+city_total_list.to_csv('city_total_list.csv', encoding='utf-8')
+"""
 #爬取中国每日疫情情况数据
+"""
 china_everday_data = spider_data("https://view.inews.qq.com/g2/getOnsInfo?name=disease_other")
 chinaDayList = json.loads(china_everday_data)["chinaDayList"]
 
 chinaDayData = [(i["date"],i["confirm"],i["dead"],i["heal"]) for i in chinaDayList]
-chinaDayData = pandas.DataFrame(chinaDayData)
+chinaDayData = pd.DataFrame(chinaDayData)
 new_cols3 = ['日期', '确诊','死亡','痊愈']
 chinaDayData.columns = new_cols3
 chinaDayData.to_csv('chinaDayData.csv', encoding='utf-8')
-
 
